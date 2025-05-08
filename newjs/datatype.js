78ffb51350f9dd5e170bf5cff151e94c777bb88d
FalguniSuryawanshi <168903119+FalguniSuryawanshi@users.noreply.github.com>
2025-05-08 09:55:54 +0000

    Add newjs files with JavaScript examples

---
+++ newjs/datatype.js
@@ -0,0 +1,11 @@
+let IsLoggedIn = 1 
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let booleanIsLoggedIn = Boolean (IsLoggedIn)
+console.log(booleanIsLoggedIn)
---
+++ newjs/string.js
@@ -0,0 +1,49 @@
+
+// let value = 3
+// let negvalue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = "fs"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+//...............//
+
+
+const name = "falu"
+const repoCount = 40
+
+console.log(name + repoCount + "value")
+
+console.log(`hello my name is  ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String("falu")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-2,3)
+console.log(anotherString);
+
+const newStringOne = "    tiger  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https:/hitesh.com/hitesh%20chaudhri"
+
+console.log(url.replace('%20','_'));
+
+
+
+
+
+
